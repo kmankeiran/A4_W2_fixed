27237ca38661243748c90a243f4abd9e82c5cf57
Hollenbeck <k_hollenbeck@ats-hr420-dm24.fc.ca>
2020-01-15 10:01:18 -0500

    base build

---
+++ js/main.js
@@ -0,0 +1,24 @@
+// todo => use a key to track the current video, or just pass the video in as a ref to the function and grab its source
+
+var vm = new Vue({
+  el: "#app",
+
+  data: {
+
+    // mock up the user - this well eventually come from the database UMS (user management system)
+
+
+    // this data would also come from the database, but we'll just mock it up for now
+    videodata: [
+      { name: "Star Wars The Force Awakens", thumb: "forceawakens.jpg", vidsource: "forceawakens.mp4", description: "yet another star wars movie" },
+      { name: "Stranger Things", thumb: "strangerthings.jpg", vidsource: "strangerthings.mp4", description: "don't get lost in the upside down" },
+      { name: "Marvel's The Avengers", thumb: "avengers.jpg", vidsource: "avengers.mp4", description: "will they make black widow action figures this time?" }
+    ],
+
+    showDetails: false
+  },
+
+  methods: {
+
+  }
+});
