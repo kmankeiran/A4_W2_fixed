d55672f5855c090492894b4d8aa2c54deef2dcc9
Hollenbeck <k_hollenbeck@ats-hr420-dm24.fc.ca>
2020-02-05 10:52:01 -0500

    adding user components and users components

--- js/main.js
+++ js/main.js
@@ -1,12 +1,40 @@
-// todo => use a key to track the current video, or just pass the video in as a ref to the function and grab its source
+// imports come first
+
+import UsersComponent from "./components/UsersComponent.js";
+
+Vue.component('player', {
+  props: ['movie'],
+
+  template: `
+  <div>
+    <h3 class="movie-title">{{ movie.videotitle }}</h3>
+    <video :src="'video/' + movie.vidsource" controls autoplay></video>
+    <div class="movie-details">
+      <p>{{ movie.videodescription }}</p>
+    </div>
+  </div>
+  `
+})
+
+const router = new VueRouter({
+  routes: [
+    { path: "/", name: "home", component: UsersComponent }
+  ]
+})
+
 
 var vm = new Vue({
   el: "#app",
 
+  router,
+
   data: {
 
     // mock up the user - this well eventually come from the database UMS (user management system)
-
+    user: {
+      isLoggedIn: true,
+      settings: {}
+    },
 
     // this data would also come from the database, but we'll just mock it up for now
     videodata: [
@@ -15,10 +43,58 @@ var vm = new Vue({
       { name: "Marvel's The Avengers", thumb: "avengers.jpg", vidsource: "avengers.mp4", description: "will they make black widow action figures this time?" }
     ],
 
+    movie: {
+      videotitle: "video title goes here",
+      vidsource: "",
+      videodescription: "video description here",
+    },
+
     showDetails: false
   },
 
-  methods: {
+  created: function() {
+    // run a fetch call and get the user data
+    console.log('created lifecycle hook fired here, go get user data');
+    //this.getUserData();
+  },
 
+  methods: {
+    getUserData() {
+      // do a fetch call here and get the user from the DB
+      const url = './includes/index.php?getUser=1';
+
+      fetch(url) // get data from the DB
+      .then(res => res.json()) // translate JSON from DB to plain object
+      .then(data => { // use the plain data object (the user)
+        console.log(data); // log it to the console (testing)
+
+        // put our DB data into vue
+        this.user.settings = data[0];
+      })
+      .catch((error) => console.error(error))
+    },
+
+    userLogin() {
+      // call the login route, or just mock up the funtionality for now
+      console.log('do login on click');
+
+      this.user.isLoggedIn = (this.user.isLoggedIn) ? false : true;
+    },
+
+    setUserPreferences() {
+      // set the user prefs if user has admin rights
+      console.log('set prefs here');
+    },
+
+    showMovieDetails({ name, description, vidsource }) {
+      // set the observables / bound data so that the view updates
+      // console,log('show these details: ', movie);
+
+      this.movie.videotitle = name;
+      this.movie.vidsource = vidsource;
+      this.movie.videodescription = description;
+
+      this.showDetails = true;
+    }
   }
 });
